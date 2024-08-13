5e351ca3b32dfc2556f7611f1dc085b877b1a6c4
RewDev <orbitamcnetwork@gmail.com>
2024-08-12 20:07:06 -0600

    Viendo las distintas formas de declarar variables

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -3,3 +3,12 @@ console.log('¡Hola, JavaScript!')
 // COMENTARIO
 /* COMENTARIO 
 MULTI LINEA*/
+
+console.log('5')
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
---
+++ Basic/01-variables.js
@@ -0,0 +1,19 @@
+// var
+
+/*var helloWorld = '¡Hello JS!'
+console.log(helloWorld)
+
+helloWorld = "New Hello JS"
+console.log(helloWorld)
+*/
+
+// let
+
+let helloWorld = '¡Hello JS!'
+console.log(helloWorld)
+
+
+// const
+
+const helloWorld2 = '¡Hello JS!'
+console.log(helloWorld2)
